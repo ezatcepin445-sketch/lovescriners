4a640d4ced7facfd2241c972095c05b7e764382d
ezatcepin445-sketch <ezatcepin445@gmail.com>
2026-01-09 20:50:30 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "LoveScriner",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
 
