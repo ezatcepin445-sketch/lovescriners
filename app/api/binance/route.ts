d0f48178fce4cbfbad0f38df5fbcf88bc25a55e1
ezatcepin445-sketch <ezatcepin445@gmail.com>
2026-01-09 22:57:58 +0300

    Update route.ts

--- app/api/binance/route.ts
+++ app/api/binance/route.ts
@@ -1,1 +1,11 @@
+import { NextResponse } from "next/server";
 
+export async function GET() {
+  const res = await fetch(
+    "https://api.binance.com/api/v3/ticker/24hr",
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+  return NextResponse.json(data);
+}
